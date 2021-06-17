9e06247066c0c9279bd667a9ba64ebc9cf187d79
이승우 <seungwoo3333333@gmail.com>
2021-06-17 15:00:37 +0900

    IPchange

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '64-_hj3%!(bxrs*%%s35w6s9-gfkv8+99d^3ab48#x43f@2jc$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['13.209.158.173']
+ALLOWED_HOSTS = ['3.37.185.253']
 #ALLOWED_HOSTS = []
 
 
